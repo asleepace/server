7bc745cb4012f2d8966cc808081a8d97944d9221
Colin <colin_teahan@yahoo.com>
2024-07-20 22:45:51 -0700

    Clean up main

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,18 @@ use core::server::Server;
 mod core;
 
 fn main() {
+    // Process command line arguments.
     let argv = cli::process_args();
 
     println!("[serveros] argv: {:?}", argv);
 
+    // Check if the user has specified a port.
     let port = match args::parse_as_num(&argv, "--port") {
         Some(port) => port as u16,
         None => 8080,
     };
 
+    // Check if the user has specified a host.
     let host = match args::parse_as_str(&argv, "--host") {
         Some(host) => host,
         None => "localhost".to_string(),
@@ -21,10 +24,19 @@ fn main() {
 
     println!("[serveros] http://{}:{}/", host, port);
 
+    // Start the server.
     let mut server = Server::new(&host, port);
-    server.route("/", |_| println!("[serveros] serving index!"));
+
+    // Define routes.
+    server.route("/", |sr| {
+        println!("[main] serving route: /");
+        sr.send_file("index.html");
+    });
+
     server.route("/info", |sr| {
+        println!("[main] serving route: info.html");
         sr.send_file("info.html");
     });
+
     server.start();
 }
